55b318c7d0ff6c7244bedcc9df58299694ce279c
Resty Artiaga <resty.artiaga@gmail.com>
2018-02-07 20:03:24 +0800

    asdfasdf
    asdfasfd

--- src/main/java/com/mexaco/controller/UserController.java
+++ src/main/java/com/mexaco/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/resty/")
+@RequestMapping
 public class UserController {
 
 	
